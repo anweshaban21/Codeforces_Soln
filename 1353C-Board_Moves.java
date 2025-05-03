0cfc9f7564d9f22c3a0764444de5b0f57ee667ca
Anwesha Banerjee <115689662+anweshaban21@users.noreply.github.com>
2025-05-03 20:01:46 +0530

    Create 1353C-Board_Moves.java

---
+++ 1353C-Board_Moves.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        sc.nextLine();
+        while(t-->0){
+            int n=sc.nextInt();
+            // long k=sc.nextLong();
+            // long q=sc.nextLong();
+            // int one=0,zero=0;
+        
+        //char[] arr={'h','e','l','l','o'};
+        //int h=0,e=0,l=0,o=0;
+        long res=0;
+        //int c=1;
+            for(int i=1;i<=n/2;i++){
+                res+=i*1L*i;
+                
+                }
+            
+            System.out.println(res*8L);
+    }
+            
+            
+            
+            
+}  
+            
+            
+    }
+
+ 
+  
