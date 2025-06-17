2c3fec91a90b11f3a3fb0bde231662afdf705a03
Anwesha Banerjee <115689662+anweshaban21@users.noreply.github.com>
2025-06-17 17:04:20 +0530

    Create 1029B-Shrink.java

---
+++ 1029B-Shrink.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        sc.nextLine();
+        while(t-->0){
+            int n=sc.nextInt();
+            System.out.print("1 ");
+            for(int i=n;i>=2;i--){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+            
+            
+
+                
+            
+            
+        }
+    }
+}
