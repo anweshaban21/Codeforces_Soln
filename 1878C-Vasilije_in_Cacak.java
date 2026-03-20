a7ea8c448ca618e6cafeea57b6989debf1b369d7
Anwesha Banerjee <115689662+anweshaban21@users.noreply.github.com>
2026-03-20 17:48:05 +0530

    Add main class for problem solution in Cacak

---
+++ 1878C-Vasilije_in_Cacak.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class main {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+            long x = sc.nextLong();
+
+            long min = k * (k + 1) / 2;
+            long max = k * (2 * n - k + 1) / 2;
+
+            if(x >= min && x <= max)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
